6d80d5692867b38c6788b5c90539f306b37ad52b
Karmavir Joshi <karmavirj@live.com>
2024-03-29 17:15:42 +0530

    Improved error handling in main function
    The main function has been updated to handle errors more gracefully. A new function, MyFunction, has been introduced which wraps the IPanicEasily function with a defer-recover block. This allows for better control over panics and provides meaningful error messages when they occur. The main function now calls MyFunction and handles any returned errors appropriately.

--- panic/main.go
+++ panic/main.go
@@ -1,12 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func IPanicEasily() {
 	panic("I'm panicking!")
 }
 
+func MyFunction() (err error) {
+	fmt.Println("Starting my function")
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in MyFunction", r)
+			err = errors.New("I panicked easily in MyFunction")
+		}
+	}()
+	IPanicEasily()
+	fmt.Println("Ending my function")
+	return nil
+}
+
 func main() {
 	fmt.Println("Panic! in the Go App")
-	IPanicEasily()
+	if err := MyFunction(); err != nil {
+		fmt.Println("Error in MyFunction", err)
+		fmt.Println(err.Error())
+	}
 }
